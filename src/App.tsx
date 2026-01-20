b45d68beb1372e785b85cb94cbeeb9961f134edc
Jashanpreet Singh <jashanpreetsingh11@rrc.ca>
2026-01-19 21:19:44 -0600

    Integrated all the components in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import PlayerList from "./components/player-list/PlayerList";
+import TeamBuilder from "./components/team-builder/TeamBuilder";
+import MatchList from "./components/match-list/MatchList";
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Fantasy Cricket Team Builder</h1>
+      </header>
+
+      <main>
+        <PlayerList />
+        <TeamBuilder />
+        <MatchList />
+      </main>
+
+      <footer>
+        <p>Created by: Jashan, Sukhpreet, Ravdeep</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
