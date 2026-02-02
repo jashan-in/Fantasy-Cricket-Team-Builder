3f20ad9cf5bf83c72e8a2d32c30a0bb80eb4fb64
Jashanpreet Singh <jashanpreetsingh11@rrc.ca>
2026-02-02 15:33:37 -0600

    Set up shared state in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,16 @@ import PlayersPage from "./pages/PlayersPage";
 import TeamPage from "./pages/TeamPage";
 import MatchesPage from "./pages/MatchesPage";
 import StatsPage from "./pages/StatsPage";
-import "./App.css";
 
 function App() {
-  
   const [team, setTeam] = useState<string[]>([]);
-  const [matches, setMatches] = useState<string[]>([]);
 
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="players" element={<PlayersPage team={team} setTeam={setTeam} />} />
         <Route path="team" element={<TeamPage team={team} setTeam={setTeam} />} />
-        <Route path="matches" element={<MatchesPage matches={matches} setMatches={setMatches} />} />
+        <Route path="matches" element={<MatchesPage />} />
         <Route path="stats" element={<StatsPage team={team} />} />
       </Route>
     </Routes>
