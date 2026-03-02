51e83564cc1c4f560b97f9e5f13da30ff3c566c1
Ravdeep Sharma <rsharma1.academic@rrc.ca>
2026-03-02 06:07:13 -0600

    Sprint 3: Removed prop drilling and wrapped app with TeamProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import { useState, type SetStateAction } from "react";
 import Layout from "./components/layout/Layout";
 import PlayersPage from "./pages/PlayersPage";
 import TeamPage from "./pages/TeamPage";
@@ -7,17 +6,13 @@ import MatchesPage from "./pages/MatchesPage";
 import StatsPage from "./pages/StatsPage";
 
 function App() {
-  const [team, setTeam] = useState<string[]>([]);
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="players" element={<PlayersPage team={team} setTeam={setTeam} />} />
-        <Route path="team" element={<TeamPage team={team} setTeam={setTeam} />} />
-        <Route path="matches" element={<MatchesPage matches={[]} setMatches={function (_: SetStateAction<string[]>): void {
-          throw new Error("Function not implemented.");
-        } } />} />
-        <Route path="stats" element={<StatsPage team={team} />} />
+        <Route path="players" element={<PlayersPage />} />
+        <Route path="team" element={<TeamPage />} />
+        <Route path="matches" element={<MatchesPage />} />
+        <Route path="stats" element={<StatsPage />} />
       </Route>
     </Routes>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App";
+import { TeamProvider } from "./context/TeamContext";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <TeamProvider>
+        <App />
+      </TeamProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
