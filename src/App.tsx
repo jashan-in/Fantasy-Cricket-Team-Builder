9ffb06d23be0612ed4b1c27205e6e87ff4f2ffd6
Jashanpreet Singh <jashanpreetsingh11@rrc.ca>
2026-03-02 21:45:29 -0600

    Refactored app.tsx and other pages to use hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import Layout from "./components/layout/Layout";
 import PlayersPage from "./pages/PlayersPage";
 import TeamPage from "./pages/TeamPage";
@@ -7,15 +6,13 @@ import MatchesPage from "./pages/MatchesPage";
 import StatsPage from "./pages/StatsPage";
 
 function App() {
-  const [team, setTeam] = useState<string[]>([]);
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="players" element={<PlayersPage team={team} setTeam={setTeam} />} />
-        <Route path="team" element={<TeamPage team={team} setTeam={setTeam} />} />
+        <Route path="players" element={<PlayersPage />} />
+        <Route path="team" element={<TeamPage />} />
         <Route path="matches" element={<MatchesPage />} />
-        <Route path="stats" element={<StatsPage team={team} />} />
+        <Route path="stats" element={<StatsPage />} />
       </Route>
     </Routes>
   );
