59ae7ca4d58485c2368323835c3d78373a009194
Jashanpreet Singh <jashanpreetsingh11@rrc.ca>
2026-03-30 23:50:51 -0500

    Configured application routing with nested routes and navigation structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import PlayersPage from "./pages/PlayersPage";
-import TeamPage from "./pages/TeamPage";
+import MyTeamPage from "./pages/MyTeamPage";
 import MatchesPage from "./pages/MatchesPage";
 import StatsPage from "./pages/StatsPage";
+import TeamsPage from "./pages/TeamsPage";
+import TeamDetailsPage from "./pages/TeamDetailsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="players" element={<PlayersPage />} />
-        <Route path="team" element={<TeamPage />} />
+        {/* Default → Matches */}
+        <Route index element={<MatchesPage />} />
+
+        {/* Core pages */}
         <Route path="matches" element={<MatchesPage />} />
+        <Route path="players" element={<PlayersPage />} />
+        <Route path="my-team" element={<MyTeamPage />} />
         <Route path="stats" element={<StatsPage />} />
+
+        {/* Teams flow */}
+        <Route path="teams" element={<TeamsPage />} />
+        <Route path="teams/:teamName" element={<TeamDetailsPage />} />
+
+        {/* Fallback (prevents blank screen on wrong route) */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { MatchProvider } from "./context/MatchContext";
+import { TeamProvider } from "./context/TeamContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <TeamProvider>
+      <MatchProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </MatchProvider>
+    </TeamProvider>
   </StrictMode>
 );
