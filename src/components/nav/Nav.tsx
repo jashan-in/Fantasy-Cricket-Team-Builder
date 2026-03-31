96ea0c8a84782267ad622be5fc4d122b089659d9
Jashanpreet Singh <jashanpreetsingh11@rrc.ca>
2026-03-30 23:48:53 -0500

    Updated Navbar for new pages and better alignment.

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -3,9 +3,10 @@ import { NavLink } from "react-router-dom";
 export default function Nav() {
   return (
     <nav>
-      <NavLink to="/players">Players</NavLink> |{" "}
-      <NavLink to="/team">My Team</NavLink> |{" "}
       <NavLink to="/matches">Matches</NavLink> |{" "}
+      <NavLink to="/players">Players</NavLink> |{" "}
+      <NavLink to="/my-team">My Team</NavLink> |{" "}
+      <NavLink to="/teams">Teams</NavLink> |{" "}
       <NavLink to="/stats">Stats</NavLink>
     </nav>
   );
