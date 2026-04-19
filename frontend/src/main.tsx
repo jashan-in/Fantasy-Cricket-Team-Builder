98374570cc33e1ef1c3d858ce6074e0e97dae5a7
Jashanpreet Singh <jashanpreetsingh11@rrc.ca>
2026-04-18 21:14:42 -0500

    Configured folder and did initial setup for clerk authentication login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
 import App from "./App";
 import { MatchProvider } from "./context/MatchContext";
 import { TeamProvider } from "./context/TeamContext";
 
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TeamProvider>
-      <MatchProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </MatchProvider>
-    </TeamProvider>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <TeamProvider>
+        <MatchProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </MatchProvider>
+      </TeamProvider>
+    </ClerkProvider>
   </StrictMode>
 );
