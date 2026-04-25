0398dc5669e9596dde729a156f7184ce2ef4efcf
Ravdeep Sharma <rsharma1.academic@rrc.ca>
2026-04-24 22:55:37 -0500

    Updated frontend app to use verification and secure routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,29 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import Layout from "./components/layout/Layout";
+
 import PlayersPage from "./pages/PlayersPage";
 import MyTeamPage from "./pages/MyTeamPage";
 import MatchesPage from "./pages/MatchesPage";
 import StatsPage from "./pages/StatsPage";
-import TeamsPage from "./pages/TeamPage";
-import TeamDetailsPage from "./pages/TeamDetailsPage";
+import TeamsPage from "./pages/TeamsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {/* Default → Matches */}
+
+        {/* Default page */}
         <Route index element={<MatchesPage />} />
 
-        {/* Core pages */}
-        <Route path="matches" element={<MatchesPage />} />
         <Route path="players" element={<PlayersPage />} />
-
-        {/* Protected Route */}
-        <Route
-          path="my-team"
-          element={
-            <>
-              <SignedIn>
-                <MyTeamPage />
-              </SignedIn>
-
-              <SignedOut>
-                <div style={{ padding: "20px" }}>
-                  <h2>Please log in to view your team</h2>
-                </div>
-              </SignedOut>
-            </>
-          }
-        />
-
+        <Route path="my-team" element={<MyTeamPage />} />
+        <Route path="matches" element={<MatchesPage />} />
         <Route path="stats" element={<StatsPage />} />
-
-        {/* Teams flow */}
         <Route path="teams" element={<TeamsPage />} />
-        <Route path="teams/:teamName" element={<TeamDetailsPage />} />
 
-        {/* Fallback */}
+        {/* fallback */}
         <Route path="*" element={<Navigate to="/" />} />
+
       </Route>
     </Routes>
   );
