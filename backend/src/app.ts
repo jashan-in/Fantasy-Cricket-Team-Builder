64b5374929efb19ce662509f573943810db04b9e
Ravdeep Sharma <rsharma1.academic@rrc.ca>
2026-04-24 22:54:13 -0500

    Updated backend clerk auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,33 @@
-import express from "express";
+const express = require("express");
 import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import dotenv from "dotenv";
+dotenv.config();
+
 import playerRoutes from "./routes/playerRoutes";
 import matchRoutes from "./routes/matchRoutes";
 import teamRoutes from "./routes/teamRoutes";
 import statsRoutes from "./routes/statsRoutes";
+import userTeamRoutes from "./routes/userTeamRoutes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
+
 app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.use("/players", playerRoutes);
 app.use("/matches", matchRoutes);
 app.use("/teams", teamRoutes);
 app.use("/stats", statsRoutes);
+app.use("/user-team", userTeamRoutes);
 
 app.listen(3000, () => {
   console.log("Backend running on port 3000");
